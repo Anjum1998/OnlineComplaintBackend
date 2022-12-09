512db309d5598668417c2b0f53cefcd63eadc352
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-09 15:17:46 +0530

    login

--- src/main/java/com/example/OnlineComplaint_Backend/controller/OnlineController.java
+++ src/main/java/com/example/OnlineComplaint_Backend/controller/OnlineController.java
@@ -3,11 +3,13 @@ package com.example.OnlineComplaint_Backend.controller;
 import com.example.OnlineComplaint_Backend.dao.OnlineDao;
 import com.example.OnlineComplaint_Backend.model.OnlineUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,7 +17,7 @@ public class OnlineController {
 
     @Autowired
     private OnlineDao dao;
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public Map<String,String> AddUser(@RequestBody OnlineUser o)
     {
@@ -29,6 +31,27 @@ public class OnlineController {
         dao.save(o);
         HashMap<String,String> map=new HashMap<>();
         map.put("status","success");
+        return map;
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/login",consumes = "application/json",produces = "application/json")
+    public Map<String,String> UserLogin(@RequestBody OnlineUser o)
+    {
+        String email=o.getEmail().toString();
+        String password=o.getPassword().toString();
+        System.out.println(email);
+        System.out.println(password);
+        List<OnlineUser> result=(List<OnlineUser>) dao.UserLogin(o.getEmail(),o.getPassword());
+        HashMap<String,String> map=new HashMap<>();
+        if(result.size()==0)
+        {
+            map.put("status","failed");
+        }
+        else{
+            int id=result.get(0).getId();
+            map.put("userId",String.valueOf(id));
+            map.put("status","success");
+        }
         return map;
     }
 }
